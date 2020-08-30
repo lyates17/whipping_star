b5b9d9bd7c5e1de9cf4b0999ca4bfd01a2051330
Lauren Yates <lyates@mit.edu>
2020-08-30 15:28:18 -0500

    updating setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ source /grid/fermiapp/products/uboone/setup_uboone.sh;
 setup git v2_15_1; 
 setup gitflow v1_11_0; 
 setup mrb v1_16_02;
-setup root v6_10_04d -q e14:nu:prof; 
+setup root v6_12_04e -q e15:prof;
 setup cmake v3_11_4;
 setup eigen v3_3_4a
 
