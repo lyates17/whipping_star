47ceb68a5693e601ae3735fd33a1264ae68b44e1
Lauren Yates <lyates@mit.edu>
2020-09-28 16:25:10 -0500

    saving xmls and scripts related to studies of fake data sets 4 and 5

---
+++ dllee/forFakeDataStudies/set4/make_total_sens_covar.py
@@ -0,0 +1,50 @@
+from ROOT import TFile,TMatrixD
+
+# Get reweightable covariance matrix
+tag = "sens_pred"
+rewgt_input_file = "%s.SBNcovar.root" % tag
+rewgt_input_f = TFile(rewgt_input_file, "READ")
+rewght_covar = rewgt_input_f.Get("frac_covariance")
+
+# Get detsys covariance matrix
+# ... flat is the only thing that works for now...
+#detsys_input_file = ""
+sel1e1p_entry = 0.0003742756318718823
+sel1m1p_entry = 0.008285363079390817
+mixed_entry = -5.593215393234595e-05
+
+# Add them together
+N_1e1p_bins = 10
+N_1e1p_subchannels = 2
+N_1m1p_bins = 19
+N_1m1p_subchannels = 1
+output_file = "total_%s.SBNcovar.root" % tag
+output_f = TFile(output_file, "RECREATE")
+covar = TMatrixD(rewght_covar)
+for i in range(covar.GetNrows()):
+    for j in range(covar.GetNcols()):
+        #covar[i][j] += detsys_covar[i][j]
+        if ( (i<N_1e1p_bins) and (j<N_1e1p_bins) ): covar[i][j] += sel1e1p_entry
+        if ( (i>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins)
+             and (j>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins) ): covar[i][j] += sel1e1p_entry
+        if ( (i<N_1e1p_bins) and (j>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins) ): covar[i][j] += sel1e1p_entry
+        if ( (j<N_1e1p_bins) and (i>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins) ): covar[i][j] += sel1e1p_entry
+        if ( (i>=N_1e1p_subchannels*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) 
+             and (j>=N_1e1p_subchannels*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) ): covar[i][j] += sel1m1p_entry
+        if ( (i<N_1e1p_bins) and (j>=N_1e1p_subchannels*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) ):  covar[i][j] += mixed_entry
+        if ( (i>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins)
+             and (j>=N_1e1p_subchannels*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) ): covar[i][j] += mixed_entry
+        if ( (j<N_1e1p_bins) and (i>=N_1e1p_subchannels*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) ):  covar[i][j] += mixed_entry
+        if ( (j>=(N_1e1p_subchannels-1)*N_1e1p_bins) and (j<N_1e1p_subchannels*N_1e1p_bins)
+             and (i>=N_1e1p_subchannels*N_1e1p_bins) and (i<N_1e1p_subchannels*N_1e1p_bins+N_1m1p_bins) ): covar[i][j] += mixed_entry
+
+for i in range(covar.GetNrows()):
+    for j in range(covar.GetNrows()):
+        #print i, j, rewght_covar[i][j], covar[i][j], covar[i][j]-rewght_covar[i][j]
+        pass
+
+covar.Write("frac_covariance")
+output_f.Write()
+output_f.Close()
+
+print "Done!"
---
+++ dllee/forFakeDataStudies/set5/scan_v2/run_all.py
@@ -0,0 +1,47 @@
+import os,subprocess
+
+topdir = os.getcwd()
+autodir = os.path.join(topdir,"auto")
+pdir = os.path.join(topdir,"persistent")
+# Main xml, SM+1*LEE spectrum, and SM+1*LEE covariance matrix
+pxml = os.path.join(pdir,"dllee_sens_pred_set5.xml")
+pspec = os.path.join(pdir,"sens_pred.SBNspec.root")
+pcovar = os.path.join(pdir,"total_sens_pred.SBNcovar.root")
+
+scale_list = [ 0.01*i for i in range(201) ]
+#scale_list = [ 0., 1., 2., 3., 4., 5. ]
+
+
+for scale in scale_list:
+
+    tag = "{:03d}".format(int(scale*100))
+    thisdir = os.path.join(autodir,tag)
+    subprocess.call("mkdir -p %s" % thisdir, shell=True)
+    os.chdir(thisdir)
+
+    # Scale the spectrum
+    cmd = "/uboone/app/users/yatesla/sbnfit/whipping_star/build/bin/sbnfit_scale_spec "
+    cmd += "--xml %s " % pxml
+    cmd += "-i %s " % pspec
+    cmd += "-s lee -v %s -t sens" % scale
+    print cmd
+    subprocess.call(cmd, shell=True)
+
+    # Build the covariance matrices with the scaled spectra
+    cmd = "/uboone/app/users/yatesla/sbnfit/whipping_star/build/bin/sbnfit_complete_covariance "
+    cmd += "--xml %s " % pxml
+    cmd += "--tag total_sens "
+    cmd += "-s sens.SBNspec.root "  # feed in the scaled spectrum from above
+    cmd += "-c %s" % pcovar
+    print cmd
+    subprocess.call(cmd, shell=True)
+
+    #cmd = "/uboone/app/users/yatesla/sbnfit/whipping_star/build/bin/sbnfit_lee_frequentist_study --xml sens.xml --tag sens -s sens.SBNspec.root "
+    #cmd += "-b %s " % os.path.join(pdir, "leeless.SBNspec.root")
+    #cmd += "-c total_sens.SBNcovar.root -n 10000 > log.txt"
+    #print cmd
+    #subprocess.call(cmd, shell=True)
+
+os.chdir(topdir)
+
+print "Done!"
