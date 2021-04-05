357c819559f3f3327d64d43c99bea43339c41224
Lauren Yates <lyates@mit.edu>
2021-04-04 23:54:45 -0500

    minor tweaks

--- dllee/forOptimizationStudies/sensitivity_2021/run_all_main.sh
+++ dllee/forOptimizationStudies/sensitivity_2021/run_all_main.sh
@@ -6,7 +6,7 @@ env > env.txt
 # This script runs the full sensitivity scan chain, up to scraping
 for script in write_xmls.py run_all_reweight-covar.py run_all_total-covar.py run_all_constraint.py write_constr_xmls.py run_all_freq.py
 do
-  log=log_`basename $script .py`.txt
+  log=`basename $script .py`.log
   cmd="python $script"
   echo $cmd
   $cmd > $log
