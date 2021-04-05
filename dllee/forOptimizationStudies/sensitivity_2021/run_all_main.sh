de4012c77952fd90481d17a4fe38d9108ac04302
Lauren Yates <lyates@mit.edu>
2021-04-04 23:42:11 -0500

    one more script to automate the sensitivity scan

---
+++ dllee/forOptimizationStudies/sensitivity_2021/run_all_main.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Save a copy of the environment (for debugging)
+env > env.txt
+
+# This script runs the full sensitivity scan chain, up to scraping
+for script in write_xmls.py run_all_reweight-covar.py run_all_total-covar.py run_all_constraint.py write_constr_xmls.py run_all_freq.py
+do
+  log=log_`basename $script .py`.txt
+  cmd="python $script"
+  echo $cmd
+  $cmd > $log
+  stat=$?
+  echo "Script finished with status $stat"
+  #if [ $stat -ne 0 ]; then
+  #  exit $stat
+  #fi
+done
+
+# Done (success)
+echo "Done!"
